e92afb22794c6e3c98c41d4e616225e2f875ffaa
hifocus <i@focuschen.com>
2020-04-16 09:38:17 +0800

    open external links with target=blank

--- footer.php
+++ footer.php
@@ -13,6 +13,12 @@
             locationOffset: 30, //距右部偏移量
         });
     });
+    var links = document.links;
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+  if (links[i].hostname != window.location.hostname) {
+    links[i].target = '_blank';
+  } 
+}
 </script>
 </main>
 </body>
